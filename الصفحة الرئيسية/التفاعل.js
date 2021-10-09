7892deb41641e1906dc435bf2a88c739326a09d3
myfirstaccoun <85413282+myfirstaccoun@users.noreply.github.com>
2021-10-09 23:29:24 +0200

    Add files via upload

---
+++ الصفحة الرئيسية/التفاعل.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".project");
+
+function right(str, chr) {
+    return str.slice(str.length-chr,str.length);
+}
+  
+function left(str, chr) {
+    return str.slice(0, chr - str.length);
+}
+
+function goto(id) {
+    idresult = right(id,1);
+    location = `../المشروع ${idresult}/مشروع.html`;
+}
