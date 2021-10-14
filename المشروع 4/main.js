98de5a932b576fda224939251e0ac9aeb60c2f59
myfirstaccoun <85413282+myfirstaccoun@users.noreply.github.com>
2021-10-14 23:50:06 +0200

    Update main.js

--- المشروع 4/main.js
+++ المشروع 4/main.js
@@ -5,6 +5,8 @@ let n2 = document.querySelector(".num2");
 let num2;
 let reset = document.querySelector(".reset");
 let inpt = document.querySelector(".resinpt");
+let numpress = document.querySelector(".parent");
+
 
 function right(str, chr) {
     return str.slice(str.length-chr,str.length);
@@ -88,6 +90,39 @@ window.onkeydown = (pr) => {
     }
 
     if (pr.keyCode == 13) {
+        if ((inpt.value) == (num1 * num2)) {
+            bodym.style.backgroundColor = "rgb(0 , 255 , 0)";
+            setTimeout(() => {
+                n1.innerText = Math.round(Math.random() * 10,0);
+                num1 = n1.innerText;
+                n2.innerText = Math.round(Math.random() * 10,0);
+                num2 = n2.innerText;
+                inpt.value = 0;
+            },1100);
+        } else {
+            bodym.style.backgroundColor = "rgb(255 , 193 ,193)";
+        }
+
+        setTimeout(() => {bodym.style.backgroundColor = "rgb(255 , 255 , 255)";}, 1000);
+    }
+}
+
+numpress.onclick = (eo) => {
+    if (eo.target.classList == "num") {
+        if ((eo.target.innerText >= 0 && eo.target.innerText <= 9 || (eo.target.innerText == 0 && inpt.value != "")) && inpt.value + eo.target.innerText < 1000 && inpt != document.activeElement) {
+            if (inpt.value == "0") {
+                inpt.value = eo.target.innerText;
+            } else {
+                inpt.value += eo.target.innerText;
+            }
+        }
+    }
+
+    if (eo.target.innerText == "⌫") {
+        inpt.value = (inpt.value / 10) - (right(inpt.value,1))/10;
+    }
+
+    if (eo.target.classList == "enter") {
         if ((inpt.value) == (num1 * num2)) {
             bodym.style.backgroundColor = "rgb(0 , 255 , 0)";
             setTimeout(() => {
