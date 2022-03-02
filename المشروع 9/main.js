240adc150cf7a510661e7bccaf96a5654aa63768
myfirstaccoun <85413282+myfirstaccoun@users.noreply.github.com>
2022-03-02 21:28:15 +0200

    Update main.js

--- المشروع 9/main.js
+++ المشروع 9/main.js
@@ -12,5 +12,5 @@ setInterval(() => {
 
     hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;
-    sc.style.transform = `rotateZ(${ss + (ms/1000)*6}deg)`;
+    sc.style.transform = `rotateZ(${ss + (ms/1000)*3}deg)`;
 });
